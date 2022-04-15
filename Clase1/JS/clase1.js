6e36565f44f0d15a85fcc8ee4ded43198f5f9458
2detrebol <maubrandi@gmail.com>
2022-04-15 19:45:47 -0300

    clases 1 y 2 (parcial)

---
+++ Clase1/JS/clase1.js
@@ -0,0 +1,12 @@
+let nombre = "Mauro";
+let apellido = "Brandi"
+const espacio = " ";
+
+let resultadoA = "Bienvenido" + espacio + nombre + espacio + apellido;
+
+console.log (resultadoA);
+
+let ingreseNombre = prompt ("Ingrese su Nombre");
+let ingreseApellido = prompt ("Ingrese su Apellido");
+
+alert ("Bienvenido/a" + espacio + ingreseNombre + " " + ingreseApellido);
