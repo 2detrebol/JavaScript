1c9af15b0f9836037c6854497bcc08db2c377b26
2detrebol <maubrandi@gmail.com>
2022-04-15 16:31:04 -0300

    agregando 1ra parte clase 2

---
+++ Clase2/JS/archivo2.js
@@ -0,0 +1,9 @@
+let ingreseNombre = prompt ("Ingrese su Nombre");
+const espacio = " ";
+
+if (ingreseNombre == "", " ") {
+    alert ("Error! NO ha ingresado su nombre. Por favor intente nuevamente");}
+else{
+    alert ("Bienvenido/a" + espacio + ingreseNombre);
+}
+
