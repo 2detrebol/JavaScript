d1da7579ec66b6f9015c6a1aeebf2694c5c8bcd1
2detrebol <maubrandi@gmail.com>
2022-05-19 09:18:31 -0300

    con comentarios - falta no repetir linea carrito

--- JS/clase5.js
+++ JS/clase5.js
@@ -1,3 +1,4 @@
+//FUNCION CONSTRUCTORA DE PRODUCTOS//
 function Producto(id, imagen, tittle, descripcion, precio, stock) {
     this.id = id;
     this.imagen = imagen;
@@ -13,28 +14,27 @@ function Producto(id, imagen, tittle, descripcion, precio, stock) {
     };
 }
 
+//CREACION DE PRODUCTOS//
 const listaProductos = [
     new Producto(1, "./IMAGENES/Items/micros/i7.png", "Intel i7", "Intel Core i7-9700K 8 núcleos y 4.9 GHz de frecuencia gráfica int.", 60000, 5),
     new Producto(2, "./IMAGENES/Items/micros/i9.png", "Intel i9", "Intel Core i9-10900F 10 núcleos y 5.2 GHz de frecuencia, gráfica int.", 80000, 20),
     new Producto(3, "./IMAGENES/Items/micros/R7.png", "AMD Ryzen 7", "AMD Ryzen 7 5700G 8 núcleos y 4.6 GHz con gráfica integrada", 55000, 15),
     new Producto(4, "./IMAGENES/Items/micros/R9.png", "AMD Ryzen 9", "AMD Ryzen 9 5900X 12 núcleos 4.8 GHz y 24 hilos con gráfica integrada", 75000, 30),
-
     new Producto(5, "./IMAGENES/Items/placasv/RTX3080.png", "RTX 3080 MSI", "MSI NVIDIA GeForce RTX3080 Gaming X Trio 10Gb GDDR6X", 300000, 10),
     new Producto(6, "./IMAGENES/Items/placasv/RTX3090.png", "RTX 3090 TUF", "ASUS NVIDIA GeForce RTX3090 TUF GAMING 24GB GDDR6X", 600000, 20),
     new Producto(7, "./IMAGENES/Items/placasv/RX6800.png", "RX 6800", "Sapphire Radeon RX 6800 NITRO+ 16Gb GDDR6", 260000, 5),
     new Producto(8, "./IMAGENES/Items/placasv/RX6900.png", "RX 6900", "MSI Radeon RX 6900 XT GAMING Z TRIO 16Gb GDDR6", 320000, 15),
-
     new Producto(9, "./IMAGENES/Items/mothers/Z690.png", "ASUS ROG MAXIMUS Z690 HERO", "Intel® Z690 ATX motherboard with 20+1 power stages, DDR5, Five M.2, USB 3.2 Gen 2x2 front-panel connector, Dual Thunderbolt™, PCIe® 5.0, Onboard WiFi 6E & Aura Sync RGB", 100000, 10),
     new Producto(10, "./IMAGENES/Items/mothers/X570S.png", "AORUS X570S ELITE", "GIGABYTE X570S AORUS ELITE - AMD Ryzen™, Twin 12+2 Phases Digital VRM Solution with 60A DrMOS, DDR4, SuperSpeed USB 3.2 Gen 2x2 TYPE-C® delivers up to 20Gb/s transfer speeds", 65000, 20),
     new Producto(11, "./IMAGENES/Items/mothers/Z690PLUS.png", "ASUS Z690PLUS TUF GAMING", "Intel® Z690 (LGA 1700) ATX gaming motherboard, PCIe® 5.0, DDR4, four M.2 slots, WiFi 6 and Intel 2.5 Gb Ethernet, Thunderbolt™ 4 support and Aura Sync RGB", 50000, 10),
     new Producto(12, "./IMAGENES/Items/mothers/B550.png", "GIGABYTE B550 AORUS PRO AC 5 PCIE", "Supports AMD Ryzen™, DDR4, Ultra Durable™ PCIe 4.0, AMP-UP Audio, USB 3.2 Gen2 Type-C™ & HDMI Support, RGB FUSION 2.0 Supports Addressable LED & RGB LED Strips", 35000, 20),
-
     new Producto(13, "./IMAGENES/Items/ram/ballistix16gb.png", "DDR4 - 16Gb BALLISTIX", "Memoria Ram DDR4 - 16Gb 3200 Mhz Ballistix Rgb Negro", 13500, 25),
     new Producto(14, "./IMAGENES/Items/ram/renegade16gb.png", "DDR4 - 16Gb KINGSTON RENEGADE", "Memoria Ram DDR4 - 16Gb 3600 Mhz Renegade Kingston Fury Rgb", 15000, 15),
     new Producto(15, "./IMAGENES/Items/ram/fury8gb.png", "DDR5 - 8Gb KINGSTON FURY", "Memoria Ram DDR5 - 8Gb 5600 Mhz Beast Kingston Fury", 16500, 25),
     new Producto(16, "./IMAGENES/Items/ram/fury16gb.png", "DDR5 - 16Gb KINGSTON FURY", "Memoria Ram DDR5 - 16Gb 5600 Mhz Beast Kingston Fury", 29000, 15),
 ]
 
+//ARRAYS PARA HTML//
 const microprocesador = [];
 microprocesador.push(listaProductos[0], listaProductos[1], listaProductos[2], listaProductos[3]);
 
@@ -47,8 +47,7 @@ motherboards.push(listaProductos[8], listaProductos[9], listaProductos[10], list
 const memoriasRam = [];
 memoriasRam.push(listaProductos[12], listaProductos[13], listaProductos[14], listaProductos[15]);
 
-//PARA MOSTRAR EN EL HTML//
-
+//MOSTRAR EN EL HTML//
 let contenedorCards1 = document.createElement("div");
 contenedorCards1.className = "accordion-body d-flex row justify-content-center"
 microprocesador.forEach((element) => {
@@ -63,7 +62,6 @@ microprocesador.forEach((element) => {
       </div>                            
   </div>`;
 });
-
 document.querySelector("#contenedorMicros").appendChild(contenedorCards1);
 
 
@@ -81,7 +79,6 @@ placaVideo.forEach((element) => {
       </div>                            
   </div>`;
 });
-
 document.querySelector("#contenedorPlacasV").appendChild(contenedorCards2);
 
 let contenedorCards3 = document.createElement("div");
@@ -98,7 +95,6 @@ motherboards.forEach((element) => {
       </div>                            
   </div>`;
 });
-
 document.querySelector("#contenedorMothers").appendChild(contenedorCards3);
 
 
@@ -116,10 +112,10 @@ memoriasRam.forEach((element) => {
       </div>                            
   </div>`;
 });
-
 document.querySelector("#contenedorRam").appendChild(contenedorCards4);
 
 
+//PARA AGREGAR AL CARRITO CON CLICKS//
 const carrito = []
 let contador = 0;
 let numeroCarrito = document.getElementById("cart_menu_num");
@@ -144,7 +140,6 @@ listaProductos.forEach((producto, index) => {
                 );
                 let listaCarrito = document.createElement("tr");
                 listaCarrito.className = "text-center align-middle"
-                listaProductos.innerHTML = "";
                 carrito.forEach((element) => {
                     listaCarrito.innerHTML += `
                     <th>${element.id}</th>
