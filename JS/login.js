75ef0073f5ecd7b5d28ba79981c4676e669bc630
2detrebol <maubrandi@gmail.com>
2022-06-04 11:46:42 -0300

    se elimina definitivamente login de index

--- JS/login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('botonLogin').addEventListener("click", function () {
-    document.getElementById('popup').classList.add("active");
-});
-
-document.querySelector('.close-btn').addEventListener("click", function () {
-    document.getElementById('popup').classList.remove("active");
-});
